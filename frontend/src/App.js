10b1ab0de984ab4696dca6afe1bdbdebd3426f0b
MatthewMcCarthy1 <127790941+MatthewMcCarthy1@users.noreply.github.com>
2025-02-25 22:22:28 +0000

    added styling to main page for registering and logging in. Page now has a basic modern look with toggle for log in and register forms.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import HomePage from "./components/HomePage";
+import './App.css'
 
 function App() {
   // State to keep track of the user that is logged in
   const [user, setUser] = useState(null);
+
+  const [isLoginActive, setIsLoginActive] = useState(true);
 
   // Effect to check for a stored user in localStorage on component mount
   useEffect(() => {
@@ -21,19 +24,44 @@ function App() {
     localStorage.setItem('user', JSON.stringify(userData));
   };
 
+  const toggleAuth = () => {
+    setIsLoginActive(!isLoginActive);
+  };
+
+  if (user) {
+    return <HomePage user={user} setUser={setUser} />;
+  }
+
   return (
-    <div className="App">
-      {/* Conditional rendering based on user authentication state */}
-      {!user ? (
-        <>
-          {/* Show Login and Register components if no user is logged in */}
-          <Login setUser={handleSetUser} />
+    <div className="auth-container">
+      <div className="auth-header-container">
+        <h1 className="auth-header">Welcome to UniStack</h1>
+      </div>
+      <div className={`auth-box ${isLoginActive ? 'login-active' : ''}`}>
+        <div className="auth-form register-form">
           <Register setUser={handleSetUser} />
-        </>
-      ) : (
-        // Show HomePage if a user is logged in
-        <HomePage user={user} setUser={setUser} />
-      )}
+        </div>
+        <div className="auth-form login-form">
+          <Login setUser={handleSetUser} />
+        </div>
+        <div className="slider">
+          <div className="slider-text">
+            {isLoginActive ? (
+              <>
+                <h2>New Here?</h2>
+                <p>Sign up now to access </p>
+                <button onClick={toggleAuth}>Sign Up</button>
+              </>
+            ) : (
+              <>
+                <h2>Welcome Back!</h2>
+                <p>Login to access your account</p>
+                <button onClick={toggleAuth}>Sign In</button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
