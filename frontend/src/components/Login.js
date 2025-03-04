81bd6cef2036f63a64f4d2d827427e88a128f089
MatthewMcCarthy1 <127790941+MatthewMcCarthy1@users.noreply.github.com>
2025-03-04 19:00:16 +0000

    added show password toggle to login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import '../components-css/Login.css';
 
 function Login({ setUser }) {
   // State for form inputs and error message
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   // Function to handle form submission
   const handleSubmit = async (e) => {
@@ -43,13 +45,19 @@ function Login({ setUser }) {
         required
       />
       {/* Password input field */}
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
+      <div className="password-container">
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <i
+          className={`fas ${showPassword ? 'fa-eye' : 'fa-eye-slash'}`}
+          onClick={() => setShowPassword(!showPassword)} 
+        ></i>
+      </div>
       <button type="submit">Login</button>
     </form>
   );
